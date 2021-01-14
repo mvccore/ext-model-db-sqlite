89501713d75578787d8cb3890e4713bc03a4995c
Tom Flídr <tomflidr@gmail.com>
2021-01-14 15:33:19 +0100

    base

---
+++ src/MvcCore/Ext/Models/Db/Models/SQLites/Features.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * MvcCore
+ *
+ * This source file is subject to the BSD 3 License
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that are distributed with this source code.
+ *
+ * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @license  https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
+ */
+
+namespace MvcCore\Ext\Models\Db\Models\SQLites;
+
+trait Features {
+
+	use \MvcCore\Ext\Models\Db\Model\Props;
+	use \MvcCore\Ext\Models\Db\Model\DataMethods;
+	use \MvcCore\Ext\Models\Db\Model\Manipulation;
+	
+	use \MvcCore\Model\Connection, 
+		\MvcCore\Ext\Models\Db\Model\Connection {
+			\MvcCore\Ext\Models\Db\Model\Connection::GetConnection insteadof \MvcCore\Model\Connection;
+			\MvcCore\Model\Connection::GetConnection as GetProviderConnection;
+		}
+
+	use \MvcCore\Model\MetaData,
+		\MvcCore\Ext\Models\Db\Model\MetaData {
+			\MvcCore\Ext\Models\Db\Model\MetaData::getMetaData insteadof \MvcCore\Model\MetaData;
+			\MvcCore\Ext\Models\Db\Model\MetaData::parseMetaData insteadof \MvcCore\Model\MetaData;
+			\MvcCore\Ext\Models\Db\Model\MetaData::parseMetaDataProperty insteadof \MvcCore\Model\MetaData;
+			\MvcCore\Model\MetaData::parseMetaDataProperty as parseMetaDataPropertyBase;
+	}
+	
+	use \MvcCore\Model\Parsers,
+		\MvcCore\Ext\Models\Db\Model\Parsers {
+			\MvcCore\Ext\Models\Db\Model\Parsers::parseToTypes insteadof \MvcCore\Model\Parsers;
+			\MvcCore\Ext\Models\Db\Model\Parsers::parseToType insteadof \MvcCore\Model\Parsers;
+			\MvcCore\Ext\Models\Db\Model\Parsers::parseToDateTime insteadof \MvcCore\Model\Parsers;
+			\MvcCore\Model\Parsers::parseToDateTime as parseToDateTimeDefault;
+	}
+	
+	use \MvcCore\Ext\Models\Db\Models\SQLites\ProviderResource;
+}
---
+++ src/MvcCore/Ext/Models/Db/Models/SQLites/ProviderResource.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * MvcCore
+ *
+ * This source file is subject to the BSD 3 License
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that are distributed with this source code.
+ *
+ * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @license  https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
+ */
+
+namespace MvcCore\Ext\Models\Db\Models\SQLites;
+
+trait ProviderResource {
+	
+	/**
+	 * Connection class full name, specific for each extension.
+	 * @var string
+	 */
+	protected static $providerConnectionClass = '\\MvcCore\\Ext\\Models\\Db\\Providers\\Connections\\SQLite';
+	
+	/**
+	 * Database provider specific resource class instance with universal SQL statements.
+	 * @var \MvcCore\Ext\Models\Db\Providers\Resources\SQLite
+	 */
+	protected static $editProviderResource = NULL;
+
+	/**
+	 * Get database provider specific resource class instance with universal SQL statements.
+	 * @return \MvcCore\Ext\Models\Db\Providers\Resources\SQLite
+	 */
+	protected static function getEditProviderResource () {
+		if (self::$editProviderResource === NULL)
+			self::$editProviderResource = new \MvcCore\Ext\Models\Db\Providers\Resources\SQLite;
+		return self::$editProviderResource;
+	}
+}
